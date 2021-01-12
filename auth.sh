62e40eceff440f44a3622e3daa271e4726d21029
jawabuu <49921621+jawabuu@users.noreply.github.com>
2021-01-12 08:24:11 +0000

    Fix auth script

--- auth.sh
+++ auth.sh
@@ -2,8 +2,9 @@
 
 case $1 in
 clear)
- gcloud -q config unset auth/impersonate_service_account
+ ##gcloud -q config unset auth/impersonate_service_account
  unset GOOGLE_OAUTH_ACCESS_TOKEN
+ unset GOOGLE_IMPERSONATE_SERVICE_ACCOUNT
  ;;
 *)
  # Set the project
@@ -11,9 +12,10 @@ clear)
     SERVICE_ACCOUNT=$(gcloud iam service-accounts list  --filter="email ~ project-service-account" --format='value(email)')
     PROJECT=$(echo $SERVICE_ACCOUNT | grep -o -P '(?<=@).*(?=.iam)')
     # Impersonate the service account
-    gcloud -q config set auth/impersonate_service_account $SERVICE_ACCOUNT
+    ##gcloud -q config set auth/impersonate_service_account $SERVICE_ACCOUNT
     # Set the token
-    GOOGLE_OAUTH_ACCESS_TOKEN=$(gcloud auth print-access-token)
+    export GOOGLE_IMPERSONATE_SERVICE_ACCOUNT=$SERVICE_ACCOUNT
+    export GOOGLE_OAUTH_ACCESS_TOKEN=$(gcloud auth print-access-token)
  else
     echo "Run . ./auth.sh [project_id]."
  fi
