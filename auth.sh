f4b4a76b5eac19b7797ef1d9fda13e169460ab99
jawabuu <49921621+jawabuu@users.noreply.github.com>
2021-01-13 11:49:25 +0000

    Restrict dns hosts changes to cloudshell

--- auth.sh
+++ auth.sh
@@ -5,7 +5,7 @@ clear)
  unset GOOGLE_OAUTH_ACCESS_TOKEN
  unset GOOGLE_IMPERSONATE_SERVICE_ACCOUNT
  # Remove user hosts entries
- sed '/##\[USER/{:a;N;/END\]##/!ba};//d' /etc/hosts | sudo tee /etc/hosts
+ [[ $CLOUD_SHELL == true ]] && sed '/##\[USER/{:a;N;/END\]##/!ba};//d' /etc/hosts | sudo tee /etc/hosts
  ;;
 *)
  # Set the project
@@ -15,17 +15,21 @@ clear)
     # Set the token
     export GOOGLE_IMPERSONATE_SERVICE_ACCOUNT=$SERVICE_ACCOUNT
     export GOOGLE_OAUTH_ACCESS_TOKEN=$(gcloud auth print-access-token)
-    # Error: dial tcp ... connect: cannot assign requested address
-    # https://github.com/hashicorp/terraform-provider-google/issues/6782
-    APIS="googleapis.com www.googleapis.com storage.googleapis.com iam.googleapis.com container.googleapis.com cloudresourcemanager.googleapis.com"
-    sed '/##\[USER/{:a;N;/END\]##/!ba};//d' /etc/hosts | sudo tee /etc/hosts.temp
-    echo '##[USER_HOSTS_START]##' | sudo tee -a /etc/hosts.temp
-    for i in $APIS
-    do
-        echo "199.36.153.10 $i" | sudo tee -a /etc/hosts.temp
-    done
-    echo '##[USER_HOSTS_END]##' | sudo tee -a /etc/hosts.temp
-    sudo cp /etc/hosts.temp /etc/hosts
+    
+    if [[ $CLOUD_SHELL == true ]]; then
+      # Error: dial tcp ... connect: cannot assign requested address
+      # https://github.com/hashicorp/terraform-provider-google/issues/6782
+      APIS="googleapis.com www.googleapis.com storage.googleapis.com iam.googleapis.com container.googleapis.com cloudresourcemanager.googleapis.com"
+          
+      sed '/##\[USER/{:a;N;/END\]##/!ba};//d' /etc/hosts | sudo tee /etc/hosts.temp
+      echo '##[USER_HOSTS_START]##' | sudo tee -a /etc/hosts.temp
+      for i in $APIS
+      do
+          echo "199.36.153.10 $i" | sudo tee -a /etc/hosts.temp
+      done
+      echo '##[USER_HOSTS_END]##' | sudo tee -a /etc/hosts.temp
+      sudo cp /etc/hosts.temp /etc/hosts
+    fi
  else
     echo "Run . ./auth.sh [project_id]."
  fi
