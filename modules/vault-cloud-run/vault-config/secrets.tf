6c6336d7f48d14c7aa839e82e719498bf6063447
jawabuu <49921621+jawabuu@users.noreply.github.com>
2021-01-21 06:55:58 +0300

    Sleep before creating kv secret

--- modules/vault-cloud-run/vault-config/secrets.tf
+++ modules/vault-cloud-run/vault-config/secrets.tf
@@ -3,9 +3,14 @@ resource "random_password" "example_secret" {
   special = false
 }
 
+resource "time_sleep" "wait_60_seconds" {
+  depends_on = [vault_mount.kvv2]
+
+  create_duration = "60s"
+}
 
 resource "vault_generic_secret" "example" {
-  depends_on = [vault_mount.kvv2]
+  depends_on = [time_sleep.wait_60_seconds]
   path       = "secret/accounts/gcp"
 
   data_json = <<EOT
